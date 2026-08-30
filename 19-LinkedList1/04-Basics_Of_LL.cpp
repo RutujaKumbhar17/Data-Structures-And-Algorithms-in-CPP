e0e059660b285c56fb4b328b13c76f8da5068513
Rutuja Kumbhar <rutujakumbhar.prof@gmail.com>
2026-08-30 21:41:44 +0530

    Enhance LinkedList class with insert and search functions; update comments for clarity

--- 19-LinkedList1/04-Basics_Of_LL.cpp
+++ 19-LinkedList1/04-Basics_Of_LL.cpp
@@ -31,14 +31,9 @@ class List{
             head = tail = NULL;
         }
 
-        //LL functions that are also availabe in STL;
-        //1) push_front
-        //2) pop_front
-        //3) push_back
-        //4) pop_back
-
-
-        // 1] PUSH FRONT
+        //LL FUNCTIONS:
+        
+        // 1] PUSH FRONT -- TC-> O(1)
 
         void push_front(int val){
             Node* newNode = new Node(val); // Dynamic
@@ -59,7 +54,8 @@ class List{
 
         }
 
-        // 2] PUSH BACK
+
+        // 2] PUSH BACK -- O(1)
 
         void push_back(int val){
             Node* newNode = new Node(val);
@@ -73,7 +69,8 @@ class List{
             }
         }
 
-        // 3] POP FRONT
+
+        // 3] POP FRONT -- O(1)
 
         void pop_front(){
             if(head == NULL){
@@ -86,7 +83,8 @@ class List{
             delete temp;
         }
 
-        // 4] POP BACK
+
+        // 4] POP BACK -- O(n)
 
         void pop_back(){
             if(head == NULL){
@@ -103,6 +101,56 @@ class List{
             tail = temp; 
         }
 
+
+        // 5] INSERT AT A SPECIFIC POSITION -- O(n)
+
+        void insert(int val,int pos){
+            if(pos<0){
+                cout<<"Invalid position\n";
+                return;
+            }
+             
+            if( pos == 0){
+                push_front(val);
+                return;
+            }
+
+            Node* temp =head;
+            for (int i = 0; i<pos-1; i++){
+                if(temp==NULL){
+                    cout<<"Invalid pos\n";
+                    return;
+                }
+                temp= temp->next;
+            }
+
+            Node* newNode =new Node(val);
+            newNode->next =temp->next;
+            temp->next =newNode;
+
+
+        }
+
+
+        // 6] SEARCH -- O(n)
+
+        int search(int key){
+            Node* temp =head;
+            int idx =0;
+
+            while(temp!= NULL){
+                if(temp->data ==key){
+                    return idx;
+                }
+
+                temp= temp->next;
+                idx++;
+
+            }
+            return -1;
+        }
+
+        // 7] PRINT THE LL -- O(n)
 
         void printll(){
             Node* temp =head;
@@ -125,6 +173,10 @@ int main(){
     ll.push_back(4);
     ll.pop_front();
     ll.pop_back();
+
+    ll.insert(4,2);
+
+    cout<<ll.search(4)<<endl;
 
     ll.printll();
 
